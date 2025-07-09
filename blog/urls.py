2db46a11e2ca21123f995df97d79e2e4a30cbb0e
tuuganbekov <48891404+tuuganbekov@users.noreply.github.com>
2025-07-09 20:08:02 +0600

    change blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PostListView, create_user
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('list/', PostListView.as_view(), name='post_list'),
     path('create/', create_user)
 ]
